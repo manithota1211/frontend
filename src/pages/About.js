5b818761cd30af4379f7aa818b1d0d92937f9d6d
manithota1211 <thotamani39@gmail.com>
2025-09-18 11:59:59 +0530

    feront end code

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Vibefeed</h1>
+      <p>This page contains information about your app.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Welcome to Vibefeed Home</h1>
+      <p>This is the homepage of your minimal frontend app.</p>
+    </div>
+  );
+}
+
+export default Home;
